db6ce0246a82ac63ac1d3b3938a6fdae545193f8
tacabservice-crypto <tacabservice@gmail.com>
2026-08-11 13:49:43 +0300

    Initial commit

--- server.ts
+++ server.ts
@@ -114,7 +114,7 @@ let auth: Auth | null = null;
 function getFirebaseServiceAccount() {
   const projectId = process.env.FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+  const privateKey: string = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "";
 
   if (projectId && clientEmail && privateKey) {
     return {
